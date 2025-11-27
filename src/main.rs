677379dc0176fe0e5c0d0429e5e3d84d4f2c00c3
GamingGuy003 <bjornfelixh@gmail.com>
2025-11-27 12:27:47 +0100

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let config = PathBuf::from("/home")
+        .join(whoami::username())
+        .join(".configures");
+    println!(
+        "Config path determined to be:  {}",
+        config.to_str().unwrap_or_default()
+    );
 }
